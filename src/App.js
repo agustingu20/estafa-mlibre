63909d563d4e8500aa29640a717cca6c99bd17ac
JimenaKatuMartinez <93katu@gmail.com>
2022-09-18 12:26:32 -0300

    Lista versión mobile de footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Bienvenidos a Estafa Mlibre</h1>
       <h2>Clon E-commerce de ML.</h2>
+      <Footer/>
     </div>
   );
 }

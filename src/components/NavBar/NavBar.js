03756d2fe88eb56535287dc1f85e2c030a514edc
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-09-22 19:12:28 -0300

    Added user title and user icon. Added Css styles to the navbar and made it responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,35 +4,74 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faMagnifyingGlass,
   faBarsStaggered,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons';
-import { Dropdown } from 'react-bootstrap';
+import {
+  Container, Dropdown, Nav, Navbar,
+} from 'react-bootstrap';
 import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
 
 const NavBar = () => {
   return (
-    <div className="navbar-container">
-      <div className="navbar-wrapper">
-        <p className="navbar-logo">Logo Estafa Mlibre</p>
-        <div className="mx-auto">
-          <input type="text" className="search-input" placeholder="Buscar" />
-          <button className="search-button">
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="search-icon" />
-          </button>
-        </div>
-        <div className="mx-5">
-          <Dropdown>
-            <DropdownToggle className='dropdown-icon'>
-              <FontAwesomeIcon icon={faBarsStaggered} className="filter-icon" />
-            </DropdownToggle>
-            <Dropdown.Menu className='dropdown-items'>
-              <DropdownItem>Action</DropdownItem>
-              <DropdownItem>Action 2</DropdownItem>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar bg="dark" expand="lg">
+        <Container>
+        <Navbar.Brand className="navbar-logo" href="#home">
+          Logo Estafa Mlibre
+        </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            className="button-colapse"
+          />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="nav-class">
+              <div className="mx-auto d-flex justify-content-center">
+                <input
+                  type="text"
+                  className="search-input"
+                  placeholder="Buscar"
+                />
+                <button className="search-button">
+                  <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    className="search-icon"
+                  />
+                </button>
+              </div>
+              <div className='buttons-container'>
+                <div>
+                  <Dropdown>
+                    <DropdownToggle className="dropdown-icon">
+                      <FontAwesomeIcon
+                        icon={faBarsStaggered}
+                        className="filter-icon"
+                      />
+                    </DropdownToggle>
+                    <Dropdown.Menu className="dropdown-items">
+                      <DropdownItem>Action</DropdownItem>
+                      <DropdownItem>Action 2</DropdownItem>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
+                <div className="d-flex justify-content-center">
+                  <p className="user-text">User</p>
+                  <Dropdown>
+                    <DropdownToggle className="dropdown-icon-user">
+                      <FontAwesomeIcon icon={faUser} className="filter-icon" />
+                    </DropdownToggle>
+                    <Dropdown.Menu className="dropdown-items">
+                      <DropdownItem>Perfil</DropdownItem>
+                      <DropdownItem>Cerrar Sesión</DropdownItem>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 };
 

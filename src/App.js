6d19b992bf590b495583fe6999596a325a6a1e19
Pablo Julian Alonso Olivera <pepoalonso1@gmail.com>
2022-09-27 19:24:20 -0300

    spinner agregado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
 import {
   RouterProvider,
 } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/footer/Footer';
 import Router from './Router';
+import { spinner } from './hooks/sweetAlert';
 
 function App() {
+  useEffect(() => {
+    spinner();
+  }, []);
+
   return (
     <div className="App">
       <NavBar />

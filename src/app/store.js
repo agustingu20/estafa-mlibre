fa3736fac2f469a4be2928354c5d3bb03e5154cb
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-09-29 18:48:07 -0300

    onClick to dispatch id. Created productIdSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 /* istanbul ignore file */
 import { configureStore } from '@reduxjs/toolkit';
+import productIdSlice from './productIdSlice';
 import searchSlice from './searchSlice';
 
 const store = configureStore({
   reducer: {
     searchResult: searchSlice,
+    productId: productIdSlice,
   },
   devTools: true,
 });
--- src/components/SearchResultsCards/SearchResultsCards.js
+++ src/components/SearchResultsCards/SearchResultsCards.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { setProductId } from '../../app/productIdSlice';
 import SearchFilters from '../SearchFilters/SearchFilters';
 import './resultCards.css';
 
 const SearchResultsCards = () => {
   const { searchResult } = useSelector((state) => state);
+
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -25,7 +29,9 @@ const SearchResultsCards = () => {
                 <h3 className="mt-5 me-3">$ {Math.trunc(result.price)}</h3>
               </div>
               <Card.Body>
-                <Card.Title>{result.title}</Card.Title>
+                <Link style={{ textDecoration: 'none' }} onClick={() => dispatch(setProductId(result.id)) }>
+                  <Card.Title name={`${result.id}`}>{result.title}</Card.Title>
+                </Link>
                 {result.shipping.free_shipping === true && (
                   <Card.Text className="text-success fw-bold">
                     Envío Gratis

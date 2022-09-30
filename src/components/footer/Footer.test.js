a45165cf10248963e920c705192734fea9fb78f9
JimenaKatuMartinez <93katu@gmail.com>
2022-09-30 16:50:31 -0300

    Acutalize test

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -1,9 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter as Router } from 'react-router-dom';
 import Footer from './Footer';
 
 describe('Test de footer', () => {
   test('Debe renderizar footer', () => {
-    render(<Footer/>);
+    render(
+      <Router>
+        <Footer/>
+      </Router>,
+    );
     const element = screen.getByText(/para obtener información útil/i);
     expect(element).toBeInTheDocument();
   });
